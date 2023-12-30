e65d46e3e5c66629661e3ece22762447ed2dda0c
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2023-12-30 18:13:54 +0200

    minor fixes

--- doomemacs/.config/doom/init.el
+++ doomemacs/.config/doom/init.el
@@ -195,3 +195,14 @@
        :config
        ;;literate
        (default +bindings +smartparens))
+       (require 'package)
+       (custom-set-variables
+       ;; custom-set-variables was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+       '(package-archives
+          (quote
+            (("gnu" . "https://elpa.gnu.org/packages/")
+               ("melpa" . "https://melpa.org/packages/")))))
+       (package-initialize)
