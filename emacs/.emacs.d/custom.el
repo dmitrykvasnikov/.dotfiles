ded7695c17d8af1fee612f3d61d90be632aa5353
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-10-08 08:42:28 +0200

    emacs tinkering

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi))
  '(custom-safe-themes
    '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(package-selected-packages '(vertico use-package dracula-theme which-key)))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,6 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (package-initialize)
 
-;; loading theme
-(load-theme 'modus-vivendi)
 ;; enable plugins
 (which-key-mode)
 (vertico-mode)
