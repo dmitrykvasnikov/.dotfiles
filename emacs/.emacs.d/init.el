c453ce171841710164b390ebb3b90b53e16e60ca
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-10-08 08:31:16 +0200

    emacs custom file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,19 +1,11 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
- '(package-selected-packages '(vertico use-package dracula-theme which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-;; loadgin theme
+(setq custom-file "~/.emacs.d/custom.el")
+(package-initialize)
+
+;; loading theme
 (load-theme 'modus-vivendi)
 ;; enable plugins
 (which-key-mode)
 (vertico-mode)
+
+;; loading custom file
+(load-file custom-file)
