bdf17db2fd0b32c2247ec486cf0926c94f687c20
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-10-13 14:53:50 +0200

    minor fixes

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(manoj-dark))
  '(package-selected-packages '(vertico which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
