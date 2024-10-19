d6836202ca4d70c6a55fa70f9c877ecb5fe2bbc1
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-10-19 20:06:21 +0200

    minor fixes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,36 @@
 (setq custom-file "~/.emacs.d/custom.el")
+;; Visual settings
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ;; disable visible scroll-bar
+(tool-bar-mode -1)   ;; disale the toolbar
+(tooltip-mode -1)    ;; disable tooltips
+(menu-bar-mode -1)   ;; disable menu bar
+(set-fringe-mode 10)
+
+(load-theme 'modus-vivendi)
+
+;; Initialize package sourcesi
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package which-key)
+(use-package vertico)
 
 ;; enable plugins
 (which-key-mode)
