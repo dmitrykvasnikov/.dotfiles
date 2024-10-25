8941c21c9cdad0960038f734ae72b07446e68101
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-10-25 14:16:22 +0200

    minor fixes

--- doomemacs/.config/doom/config.el
+++ doomemacs/.config/doom/config.el
@@ -1,7 +1,4 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
 ;; packages
-
 (custom-set-variables
 '(package-archives
    (quote
@@ -9,18 +6,27 @@
       ("melpa" . "https://melpa.org/packages/")))))
 (package-initialize)
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Dmitry Kvasnikov"
       user-mail-address "dmitry.kvasnikov@gmail.com")
 
-(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
-(load-file "~/.config/doom/font.el")
+(setq doom-theme 'doom-tokyo-night)
 
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
+(load-file "~/.config/doom/font.el")
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/org/")
+;; setup clipboard
+;; don't put deleted strings to X11 clipboard
+(setq select-enable-clipboard nil)
+;; copying and pasting selected blocks in visual mode to and from X11 clipboard
+(map! "S-C-c" #'clipboard-kill-ring-save)
+(map! "S-C-v" #'clipboard-yank)
+;; break lines on words
+(global-visual-line-mode t)
+(setq vc-follow-symlinks nil)
+(setq auto-save-default nil)
+;;
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
@@ -36,68 +42,6 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night)
-
-;; This determines the style of line numbers in effect. If set to `nil',
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; setup clipboard
-;; don't put deleted strings to X11 clipboard
-(setq select-enable-clipboard nil)
-
-;; copying and pasting selected blocks in visual mode to and from X11 clipboard
-(map! "S-C-c" #'clipboard-kill-ring-save)
-(map! "S-C-v" #'clipboard-yank)
-
-;; break lines on words
-(global-visual-line-mode t)
-(setq vc-follow-symlinks nil)
-(setq auto-save-default nil)
 
 ;; enable ligatures
 (ligature-set-ligatures 't           '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
@@ -122,4 +66,4 @@
    ("C-c r" . ormolu-format-buffer)))
 
 (custom-set-variables '(haskell-stylish-on-save t))
-(setq lsp-ui-sideline-enable nil)
+(setq lsp-lens-enable nil)
