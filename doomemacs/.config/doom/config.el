4e9288066ef9f69b4773e4bdce4c7df42a5dabfb
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-11-10 09:43:42 +0200

    minor fixes

--- doomemacs/.config/doom/config.el
+++ doomemacs/.config/doom/config.el
@@ -18,10 +18,7 @@
 (setq lsp-enable-symbol-highlighting nil)
 (setq lsp-lens-enable nil)
 
-;; (setq doom-font (font-spec :family "Iosevka Comfy" :weight medium :size 20))
-;; (cond ((string-equal (system-name) "imac") (setq doom-font (font-spec :size 18))))
-;;
-;; (load-file "~/.config/doom/font.el")
+;; font
 (custom-set-faces '(default ((t (:height 110 :family "Iosevka Comfy")))))
 (cond ((string-equal (system-name) "imac") (custom-set-faces '(default ((t (:height 110))))))
       ((string-equal (system-name) "asus") (custom-set-faces '(default ((t (:height 110))))))
--- doomemacs/.config/doom/font.el
+++
@@ -1,3 +0,0 @@
-(custom-set-faces
-;; '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 120 :width normal :foundry "UKWN" :family "VictorMono Nerd Font")))))
- '(default ((t (:height 110 :family "Iosevka Comfy")))))
