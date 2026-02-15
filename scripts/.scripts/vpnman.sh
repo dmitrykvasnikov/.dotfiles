04503b566ad1d25b33055af61021decbdc0b1ff6
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2026-02-15 11:11:53 +0200

    VPN manager updated

--- scripts/.scripts/vpnman.sh
+++ scripts/.scripts/vpnman.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+# Variables
 SERVICE="awg-quick@wg0.service"
 INTERFACE="RT-5GPON-800C"
 
+# ANSI colors
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+BLUE="\033[0;34m"
+NC="\033[0m" # No Color
+
+clear
+echo -e "${GREEN}Amnezia vpn switcher\n${NC}"
+
 while true; do
-    STATUS=$(systemctl is-active --quiet "$SERVICE" && echo "running" || echo "stopped")
-    echo "Current state of $SERVICE: $STATUS"
-    echo "Press s to switch state, q to quit"
+    if systemctl is-active "$SERVICE" &>/dev/null; then
+        STATUS="${GREEN}running${NC}"
+        # ACTION="stop"
+    else
+        STATUS="${RED}stopped${NC}"
+        # ACTION="start"
+    fi
+    echo -e "Current state of $SERVICE: $STATUS"
+    echo -e "${BLUE}Press s to switch state, q to quit${NC}"
     read -r choice
     if [[ "$choice" == "q" ]]; then
         break
     elif [[ "$choice" == "s" ]]; then
-        if [ "$STATUS" = "running" ]; then
-            systemctl stop "$SERVICE" && nmcli con down id "$INTERFACE" && nmcli con up id "$INTERFACE"
+        if [ "$STATUS" = "${GREEN}running${NC}" ]; then
+            echo "wait ..."
+            systemctl stop "$SERVICE" && nmcli con down id "$INTERFACE" > /dev/null && nmcli con up id "$INTERFACE" > /dev/null
 
-            echo "$SERVICE stopped."
+            # echo "$SERVICE stopped."
         else
             resolvconf -u && systemctl start "$SERVICE"
-            echo "$SERVICE started."
+            # echo "$SERVICE started."
         fi
     fi
 done
